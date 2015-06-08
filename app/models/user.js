3ff8cafcfbd27029ee87d4273087a8425b9833e4
justinthareja <justin.thareja@gmail.com>
2015-06-08 16:57:59 -0700

    user model, POST method to add new user to the SQL table using bookshelf.js

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,28 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
+  tableName: 'users',
+  // hasTimestamps: true,
+
+  links: function () {
+    // do we need a foreign key for this
+    return this.belongsToMany(Link);
+  },
+  initialize: function () {
+    // this.on('creating', function (mode, attrs, options) {
+    //   // this.get('username')
+    //   // this.get('password')
+    //   // console.log(this.get('username'));
+    //   // console.log(this.get('password'));
+    //   var username = this.get('username')
+    //   var password = bcrypt.hash(this.get('password'), null, null, function (err, hash) {
+    //     this.set('password', hash);
+        // console.log(password);
+    //   })
+
+    // })
+  }
+
 });
 
 module.exports = User;
